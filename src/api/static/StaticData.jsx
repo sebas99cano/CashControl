b2eccf4732b3bc1853a5d3281ff3634679964db4
sebastian <sebas99cano@gmail.com>
2024-02-19 13:58:20 -0500

    data static para los selectores

---
+++ src/api/static/StaticData.jsx
@@ -0,0 +1,25 @@
+export const ThemeData = [
+  {
+    icon: "🌞",
+    name: "Light",
+    value: "LIGHT_MODE",
+  },
+  {
+    icon: "🌚",
+    name: "Dark",
+    value: "DARK_MODE",
+  },
+];
+
+export const LanguageData = [
+  {
+    icon: "☕",
+    name: "Español",
+    value: "SWITCH_TO_SPANISH",
+  },
+  {
+    icon: "🗽",
+    name: "English",
+    value: "SWITCH_TO_ENGLISH",
+  },
+];
